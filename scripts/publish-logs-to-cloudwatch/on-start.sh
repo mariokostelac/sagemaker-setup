6f4e6bc7725a550c8988b900698c596c43db2ce4
Mario Kostelac <mario.kostelac@gmail.com>
2020-03-14 18:00:11 +0000

    Add script to publish logs to cloudwatch

---
+++ scripts/publish-logs-to-cloudwatch/on-start.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+LOG_NAME=$1
+FILENAME=$2
+NOTEBOOK_INSTANCE_NAME=$(jq '.ResourceName' \
+                      /opt/ml/metadata/resource-metadata.json --raw-output)
+
+# make sure it exists before we create a rule
+touch $FILENAME
+
+echo "Changing awslogs configuration to send ${FILENAME}"
+has_config=$(grep "auto-stop-idle" /etc/awslogs/awslogs.conf || echo "no")
+if [[ "${has_config}" == "no" ]]; then
+    echo "adding awslogs config for ${LOG_NAME}"
+    cat >> /etc/awslogs/awslogs.conf <<EOF
+
+[${FILENAME}]
+file = ${FILENAME}
+buffer_duration = 5000
+log_stream_name = ${NOTEBOOK_INSTANCE_NAME}/${LOG_NAME}
+initial_position = end_of_file
+log_group_name = /aws/sagemaker/NotebookInstances
+EOF
+else
+    echo "awslogs already has config for ${LOG_NAME}"
+fi
+
+echo "Restaring awslogs service"
+pkill -f "/usr/bin/aws logs push" || true
+sleep 5
+service awslogs start
