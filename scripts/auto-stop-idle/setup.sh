d2c3baced2faaf435bb3df34c52dfe7401e3a2a9
Mario Kostelac <mario.kostelac@gmail.com>
2020-03-14 17:40:46 +0000

    Overwrite files if already exist

--- scripts/auto-stop-idle/setup.sh
+++ scripts/auto-stop-idle/setup.sh
@@ -3,11 +3,15 @@ set -e
 SHA=${1:-master}
 echo "Installing version ${SHA}"
 
+# create a friendly directory in persisted path
 mkdir -p /home/ec2-user/lifecycle-scripts/auto-stop-idle
 cd /home/ec2-user/lifecycle-scripts/auto-stop-idle
-wget https://raw.githubusercontent.com/mariokostelac/sagemaker-setup/${SHA}/scripts/auto-stop-idle/autostop.py
-wget https://raw.githubusercontent.com/mariokostelac/sagemaker-setup/${SHA}/scripts/auto-stop-idle/on-start.sh
+
+wget -O autostop.py https://raw.githubusercontent.com/mariokostelac/sagemaker-setup/${SHA}/scripts/auto-stop-idle/autostop.py
 chmod +x autostop.py
+
+wget -O on-start https://raw.githubusercontent.com/mariokostelac/sagemaker-setup/${SHA}/scripts/auto-stop-idle/on-start.sh
 chmod +x on-start.sh
 
+# install the watcher
 ./on-start.sh
