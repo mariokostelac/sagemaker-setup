8223ce555e1e01cf61083e413af68f18d5a28e44
Mario Kostelac <mario.kostelac@gmail.com>
2020-03-14 17:35:15 +0000

    Place the script in new dir and run it

--- scripts/auto-stop-idle/setup.sh
+++ scripts/auto-stop-idle/setup.sh
@@ -3,10 +3,11 @@ set -e
 SHA=${1:-master}
 echo "Installing version ${SHA}"
 
-#mkdir -p /home/ec2-user/lifecycle-scripts/auto-stop-idle
-#cd /home/ec2-user/lifecycle-scripts/auto-stop-idle
+mkdir -p /home/ec2-user/lifecycle-scripts/auto-stop-idle
+cd /home/ec2-user/lifecycle-scripts/auto-stop-idle
 wget https://raw.githubusercontent.com/mariokostelac/sagemaker-setup/${SHA}/scripts/auto-stop-idle/autostop.py
 wget https://raw.githubusercontent.com/mariokostelac/sagemaker-setup/${SHA}/scripts/auto-stop-idle/on-start.sh
 chmod +x autostop.py
 chmod +x on-start.sh
 
+./on-start.sh
