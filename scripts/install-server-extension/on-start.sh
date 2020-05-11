2a57f17c3b57d02c91dad9ac9ac3f57cb1000b25
Mario Kostelac <mario.kostelac@gmail.com>
2020-05-11 21:09:07 +0100

    Add script to install server extension

---
+++ scripts/install-server-extension/on-start.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo --preserve-env=PIP_PACKAGE_NAME,EXTENSION_NAME -u ec2-user -i <<'EOF'
+if [[ -z "${PIP_PACKAGE_NAME}" ]]; then
+  echo "PIP_PACKAGE_NAME is empty"
+  exit
+fi
+
+if [[ -z "${EXTENSION_NAME}" ]]; then
+  echo "EXTENSION_NAME is empty"
+  exit
+fi
+
+source /home/ec2-user/anaconda3/bin/activate JupyterSystemEnv
+pip install $PIP_PACKAGE_NAME
+jupyter serverextension enable $EXTENSION_NAME --py --sys-prefix
+source /home/ec2-user/anaconda3/bin/deactivate
+EOF
