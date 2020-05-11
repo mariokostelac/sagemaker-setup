89281a31021d606f43bf1f7610100d369a17ec4c
Mario Kostelac <mario.kostelac@gmail.com>
2020-05-11 21:15:46 +0100

    Add scripts to install lab and nb extensions

---
+++ scripts/install-lab-extension/on-start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo --preserve-env=EXTENSION_NAME -u ec2-user -i <<'EOF'
+
+if [[ -z "${EXTENSION_NAME}" ]]; then
+  echo "EXTENSION_NAME is empty"
+  exit
+fi
+
+source /home/ec2-user/anaconda3/bin/activate JupyterSystemEnv
+jupyterlab extension install ${EXTENSION_NAME}
+source /home/ec2-user/anaconda3/bin/deactivate
+EOF
